358af961251fe69668dd0da3d31bc3e81e92220b
Fernando Escandalo <fernando@ongoney.com>
2018-09-02 21:48:44 +0100

    deleted rollon and ranking

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './ui/header/Header';
 import Currencies from './ui/sections/currencies/Currencies';
 import Quiz from './ui/sections/quiz/Quiz';
-import Ranking from './ui/sections/ranking/Ranking';
-import Rollon from './ui/sections/rollon/Rollon';
 import Footer from './ui/footer/Footer';
 
 import './App.css';
@@ -17,9 +15,7 @@ class App extends Component {
         <div>
           <Header />
           <Route path="/" exact component={Currencies} />
-          <Route path="/rollon" component={Rollon} />
           <Route path="/quizlogo" component={Quiz} />
-          <Route path="/ranking" component={Ranking} />
           <Footer />
         </div>
       </BrowserRouter>
